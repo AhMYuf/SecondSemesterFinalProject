c15ec95b7d6ca0803e858557b302000f999fabf4
Ahmet Yusuf Yildirim <150823826+AhMYuf@users.noreply.github.com>
2024-05-22 09:16:11 -0400

    redoing my project 2.2 small change

--- src/main/java/org/example/files/classes/reference/CompletionStatus.java
+++ src/main/java/org/example/files/classes/reference/CompletionStatus.java
@@ -4,5 +4,6 @@ package org.example.files.classes.reference;
  * Enum class that holds all the completion status for the tasks
  */
 public enum CompletionStatus {
-    COMPLETE, INCOMPLETE, PENDING, IN_PROGRESS, NOT_STARTED, ON_HOLD, CANCELLED, ABANDONED
+    COMPLETE, INCOMPLETE, PENDING, IN_PROGRESS, NOT_STARTED, ON_HOLD, CANCELLED, ABANDONED, REPETITIVE_WEEKLY, REPETITIVE_MONTHLY
 }
+
